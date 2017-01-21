442619ed4b5d45e81cbfe9aa31ad039f278f6375
Tormod Fjeldskår <tormod.fjeldskaar@gmail.com>
2017-01-21 22:10:44 +0100

    Initial scaffolding of lexer and parser

---
+++ FsExprCompiler.Parser/Expr.fs
@@ -0,0 +1,11 @@
+﻿module Expr
+
+type value =
+    | Double of double
+    | Variable of string
+
+type op = Add | Sub | Mul | Div
+
+type expr =
+    | Value of value
+    | Expr of (expr * op * expr)
